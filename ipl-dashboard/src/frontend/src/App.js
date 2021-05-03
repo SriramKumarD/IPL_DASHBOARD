7e9325a3766dd32a10ada15011ef43a0751de261
srird <ramkumar.d94@gmail.com>
2021-05-03 12:02:08 +0530

    Completed the UI elements for TeamPage

--- ipl-dashboard/src/frontend/src/App.js
+++ ipl-dashboard/src/frontend/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import { TeamPage } from './Pages/TeamPage';
+import { MatchPage } from './Pages/MatchPage';
 
-function App() {
+/* function App() {
   return (
     <div className="App">
-      <TeamPage />
+      <Router>
+        <Route path="/teams/:teamName">
+            <TeamPage />
+        </Route>
+      </Router>
+      
     </div>
   );
 }
 
-export default App;
+export default App; */
+
+export const App = () => {
+  return (
+    <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/teams/:teamName/matches/:year">
+                <MatchPage />
+            </Route>
+            <Route path="/teams/:teamName">
+                <TeamPage />
+            </Route>
+          </Switch>
+        </Router>
+    </div>
+  );
+}
